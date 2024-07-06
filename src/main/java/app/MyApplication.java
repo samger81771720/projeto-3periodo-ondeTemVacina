ff35838cd3fb5b2111e51cda19f6abd7c6fdedb0
André Sampaio <sam_ger@hotmail.com>
2024-07-06 15:31:57 -0300

    aaa

--- src/main/java/app/MyApplication.java
+++ src/main/java/app/MyApplication.java
@@ -3,7 +3,7 @@ package app;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-// Classe necessária para utilizar as anotações do Jakarta
+// Classe necessária para utilizar as anotações do Jakarta.
 @ApplicationPath("/rest")
 public class MyApplication  extends Application{
 
